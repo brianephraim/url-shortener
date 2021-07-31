4189896432349928a823b76827f65bf12549371b
Brian Ephraim <brianephraim@gmail.com>
2021-07-31 12:13:36 -0400

    more refactoring to typescript

--- app/UrlShortenerHistory.tsx
+++ app/UrlShortenerHistory.tsx
@@ -3,22 +3,40 @@ import {ActivityIndicator} from 'react-native';
 import useBellyApi from './useBelyApi';
 import ShortenedUrlItem from './ShortenedUrlItem';
 
+
+interface ShortenedUrlsData {
+  short_url: string;
+  url: string;
+  slug: string;
+  added: number
+}
+interface ShortenedUrls {
+  data: ShortenedUrlsData[];
+  initialized: boolean;
+
+}
+interface BellyApiObj {
+  isLoading: boolean;
+  refreshShortenedUrl: () => void;
+  shortenedUrls: ShortenedUrls;
+}
+
 const UrlShortenerHistory = () => {
-  const {shortenedUrls, refreshShortenedUrl} = useBellyApi('history');
-  useEffect(refreshShortenedUrl, [refreshShortenedUrl]);
+  const {shortenedUrls, refreshShortenedUrl}: BellyApiObj = useBellyApi();
+  useEffect(() => {refreshShortenedUrl()}, [refreshShortenedUrl]);
   return (
     <>
       {shortenedUrls.data
         .slice()
         .reverse()
-        .map(({short_url: shortUrl, url: longUrl, slug, added}, index) => {
+        .map(({short_url: shortUrl, url: longUrl, slug, added}: ShortenedUrlsData, index: number) => {
           return (
             <ShortenedUrlItem
               key={shortUrl}
               shortUrl={shortUrl}
               longUrl={longUrl}
               slug={slug}
-              highlighted={index === 0 && added}
+              highlighted={index === 0 && !!added}
             />
           );
         })}
